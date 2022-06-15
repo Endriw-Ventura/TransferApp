b081c85318f410665f96bb0be11f97fc72bbef5a
Endriw-Ventura <endriwventura@yahoo.com>
2022-06-14 23:28:15 -0300

    some troubles resolved with the callings and namings.

---
+++ lib/http/app_database.dart
@@ -0,0 +1,15 @@
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+import '../database/dao/contact/contact_dao.dart';
+
+Future<Database> getDatabase() async {
+  final String path = join(await getDatabasesPath(), 'bytebank.db');
+  return openDatabase(
+    path,
+    onCreate: (db, version) {
+      db.execute(ContactDao.tableSql);
+    },
+    version: 1,
+  );
+}
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,24 @@
+import 'package:cursoflutter/screens/home/dashboard.dart';
 import 'package:flutter/material.dart';
-import 'common/bytebank.dart';
-void main() =>  runApp(ByteBank());
 
+void main() {
+  runApp(BytebankApp());
+}
 
+class BytebankApp extends StatelessWidget {
 
-
-
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.green[900],
+        accentColor: Colors.blueAccent[700],
+        buttonTheme: ButtonThemeData(
+          buttonColor: Colors.blueAccent[700],
+          textTheme: ButtonTextTheme.primary,
+        ),
+      ),
+      home: Dashboard(),
+    );
+  }
+}
